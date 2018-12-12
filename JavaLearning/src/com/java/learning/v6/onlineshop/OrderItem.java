406b29bcc4929a675c43830dfd285af8e12bf9c7
olma <olma@10.153.0.223>
2018-12-12 15:13:19 +0100

    removed the constructors

--- JavaLearning/src/com/java/learning/v6/onlineshop/OrderItem.java
+++ JavaLearning/src/com/java/learning/v6/onlineshop/OrderItem.java
@@ -4,12 +4,6 @@ public class OrderItem {
 	
 	private Product product;
 	private int quantity;
-		
-	public OrderItem(Product product, int quantity) {
-
-		this.product = product;
-		this.quantity = quantity;
-	}
 	
 	public int getQuantity() {
 		return quantity;
--- JavaLearning/src/com/java/learning/v6/onlineshop/Product.java
+++ JavaLearning/src/com/java/learning/v6/onlineshop/Product.java
@@ -4,13 +4,7 @@ public class Product {
 	
 	private String name;
 	private double price;
-		
-	public Product(String name, double price) {
 
-		this.name = name;
-		this.price = price;
-	}
-	
 	public String getName() {
 		return name;
 	}
