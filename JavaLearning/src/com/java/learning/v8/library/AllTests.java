ed44b9c26ceb58ecf93194f1c95a01d31a14080d
claudiuu <claudiuu@github.com>
2018-12-18 15:00:28 +0100

    created architecture test

--- JavaLearning/src/com/java/learning/v8/library/AllTests.java
+++ JavaLearning/src/com/java/learning/v8/library/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AuthorTest.class, BookTest.class, PublisherTest.class, LibraryTest.class })
+@SuiteClasses({ AuthorTest.class, BookTest.class, PublisherTest.class, LibraryTest.class, ArchitectureTest.class })
 public class AllTests {
 
 }
---
+++ JavaLearning/src/com/java/learning/v8/library/ArchitectureTest.java
@@ -0,0 +1,34 @@
+package com.java.learning.v8.library;
+
+import static org.junit.Assert.fail;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+public class ArchitectureTest {
+
+	@Test
+	public void testThatNoClassHasPublicVariables() {
+		checkClass(BookAuthor.class);
+		checkClass(Book.class);
+		checkClass(Publisher.class);
+	}
+
+	private void checkClass(Class<?> clazz) {
+		List<Field> fields = Arrays.asList(clazz.getDeclaredFields());
+		List<Field> publicFields = fields.stream().filter(f -> (f.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC).collect(Collectors.toList());
+		if (!publicFields.isEmpty()) {
+			fail(String.format("Class '%s' should not have any public variable. The following variables are declared as public and should be private instead: %s", clazz.getName(),
+					fieldNames(publicFields)));
+		}
+	}
+
+	private String fieldNames(List<Field> publicFields) {
+		return publicFields.stream().map(f -> f.getName()).collect(Collectors.joining(", "));
+	}
+}
