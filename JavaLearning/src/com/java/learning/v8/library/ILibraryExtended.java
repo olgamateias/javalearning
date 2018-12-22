d433aea2a325f7cc115bf35a62f001771986eb70
claudiuu <claudiuu@github.com>
2018-12-22 14:37:19 +0100

    library extended

---
+++ JavaLearning/src/com/java/learning/v8/library/ILibraryExtended.java
@@ -0,0 +1,15 @@
+package com.java.learning.v8.library;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ILibraryExtended {
+
+	public Map<IAuthor, List<IBook>> getBooksByAuthor();
+
+	public Map<IPublisher, List<IBook>> getBooksByPublisher();
+
+	public Map<IPublisher, List<IAuthor>> getAuthorsForPublisher();
+
+	public Map<IAuthor, List<IPublisher>> getPublishersForAuthor();
+}
