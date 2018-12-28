9a7a41ab56e6344ebd5ab219cf431d38c77a6230
olga <olga@github.com>
2018-12-28 14:34:34 +0100

    INonAutomatedVehicle && NonAutomatedVehicle

---
+++ JavaLearning/src/com/java/learning/v11/dealer/INonAutomatedVehicle.java
@@ -0,0 +1,20 @@
+package com.java.learning.v11.dealer;
+
+public interface INonAutomatedVehicle {
+
+	void setPrice(double price);
+
+	double getPrice();
+
+	void setNrWheels(int nrWheels);
+
+	int getNrWheels();
+
+	void setWeight(double weight);
+
+	double getWeight();
+	// double weight;
+	// int nrWheels;
+	// double price;
+
+}
---
+++ JavaLearning/src/com/java/learning/v11/dealer/NonAutomatedVehicle.java
@@ -0,0 +1,39 @@
+package com.java.learning.v11.dealer;
+
+public class NonAutomatedVehicle implements INonAutomatedVehicle {
+
+	private double weight;
+	private int nrWheels;
+	private double price;
+
+	@Override
+	public double getWeight() {
+		return this.weight;
+	}
+
+	@Override
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	@Override
+	public int getNrWheels() {
+		return this.nrWheels;
+	}
+
+	@Override
+	public void setNrWheels(int nrWheels) {
+		this.nrWheels = nrWheels;
+	}
+
+	@Override
+	public double getPrice() {
+		return this.price;
+	}
+
+	@Override
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
