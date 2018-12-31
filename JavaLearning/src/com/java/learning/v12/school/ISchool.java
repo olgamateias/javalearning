a271113e65499582b29962bf1dbd825856d3e02c
claudiuu <claudiuu@github.com>
2018-12-31 13:44:05 +0100

    school

---
+++ JavaLearning/src/com/java/learning/v12/school/ISchool.java
@@ -0,0 +1,16 @@
+package com.java.learning.v12.school;
+
+public interface ISchool {
+
+	/*
+	 * We need to store the data from exams for a school. We need the following entities:
+	 * Student: first name, last name, sex, date of birth
+	 * Teacher: first name, last name, sex, date of birth, subjects (list)
+	 * Subject (enum) with values: Math, Literature, Grammar, Physics, Chemistry
+	 * Class: students (list), teachers (will also contain the principal teacher), principal
+	 * teacher, name/level (string)
+	 * Grade: student, teacher, grade (double), subject
+	 * School: all students, all teachers, all classes, all grades
+	 * 
+	 */
+}
