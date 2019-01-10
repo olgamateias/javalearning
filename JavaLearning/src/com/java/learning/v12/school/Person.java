5cea7527a28e2fa67a01e5a2d701238244b51183
olma <olma@10.153.1.1>
2019-01-10 17:06:10 +0100

    hashCode() & equals() in Person

--- JavaLearning/src/com/java/learning/v12/school/Person.java
+++ JavaLearning/src/com/java/learning/v12/school/Person.java
@@ -48,4 +48,35 @@ public abstract class Person implements IPerson {
 		this.birthDate = birthDate;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.firstName == null) ? 0 : this.firstName.hashCode());
+		result = prime * result + ((this.lastName == null) ? 0 : this.lastName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (this.firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!this.firstName.equals(other.firstName))
+			return false;
+		if (this.lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!this.lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+
 }
